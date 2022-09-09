0f3f230ee0b797dad16befb82d0e138d0c7ac323
Asifur Rahaman <110245664+procoderasif@users.noreply.github.com>
2022-09-09 15:41:48 +0600

    Summation from 1 to N

---
+++ Summation from 1 to N.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,i;
+    long long int x=0;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        x=x+i;
+    }
+    cout<<x<<endl;
+
+    return 0;
+}
